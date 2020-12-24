103cb7a82465d2130e44697b5cf7e97192b4a45b
seankross <smk240@gmail.com>
2020-12-23 17:51:30 -0800

    create_postcards can now create a placeholder image

--- R/create.R
+++ R/create.R
@@ -5,12 +5,16 @@
 #' @inheritParams rmarkdown::draft
 #' @param create_dir `TRUE` to create a new directory for the postcard
 #'   (defaults to `FALSE`).
+#' @param create_image `TRUE` to create a placeholder image
+#'   (defaults to `TRUE`).
 #'
 #' @export
-create_postcard <- function(file = "postcard.Rmd",
+create_postcard <- function(file = "index.Rmd",
                             template = "jolla",
                             create_dir = FALSE,
-                            edit = TRUE) {
+                            edit = TRUE,
+                            create_image = TRUE) {
+
   article <- rmarkdown::draft(
     file,
     template,
@@ -18,6 +22,17 @@ create_postcard <- function(file = "postcard.Rmd",
     create_dir = create_dir,
     edit = FALSE
   )
+
+  if(create_image) {
+    img_table <- as.list(
+      system.file("img",
+                  c("tobi.jpg", "xiang.jpg", "herzl.jpg", "frank.jpg"),
+                  package = "postcards"))
+
+    names(img_table) <- c("jolla", "jolla-blue", "onofre", "trestles")
+    file.copy(img_table[[template]], dirname(file))
+  }
+
   if (edit) {
     if (rstudioapi::hasFun("navigateToFile"))
       rstudioapi::navigateToFile(file)
