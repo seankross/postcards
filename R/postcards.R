5dff07315adb8da61ca2167d7a648edb59737cb4
seankross <smk240@gmail.com>
2021-02-03 14:21:05 -0800

    added self_contained option

--- R/postcards.R
+++ R/postcards.R
@@ -2,40 +2,42 @@
 #'
 #' @inheritParams rmarkdown::html_document
 #' @export
-jolla <- function(css = NULL, includes = NULL) {
-  get_template("jolla", css, includes)
+jolla <- function(css = NULL, includes = NULL, self_contained = TRUE) {
+  get_template("jolla", css, includes, self_contained)
 }
 
 #' Jolla Blue website template.
 #'
 #' @inheritParams rmarkdown::html_document
 #' @export
-jolla_blue <- function(css = NULL, includes = NULL) {
-  get_template("jolla-blue", css, includes)
+jolla_blue <- function(css = NULL, includes = NULL, self_contained = TRUE) {
+  get_template("jolla-blue", css, includes, self_contained)
 }
 
 #' Jolla Blue website template.
 #'
 #' @inheritParams rmarkdown::html_document
 #' @export
-trestles <- function(css = NULL, includes = NULL) {
-  get_template("trestles", css, includes)
+trestles <- function(css = NULL, includes = NULL, self_contained = TRUE) {
+  get_template("trestles", css, includes, self_contained)
 }
 
 #' Onofre website template.
 #'
 #' @inheritParams rmarkdown::html_document
 #' @export
-onofre <- function(css = NULL, includes = NULL) {
-  get_template("onofre", css, includes)
+onofre <- function(css = NULL, includes = NULL, self_contained = TRUE) {
+  get_template("onofre", css, includes, self_contained)
 }
 
-get_template <- function(name, css, includes) {
+get_template <- function(name, css, includes, self_contained) {
 
-  # Must we use "old" templates?
-  minimum_required <- "2.8"
-  installed <- as.character(rmarkdown::pandoc_version())
-  self_contained <- !(utils::compareVersion(minimum_required, installed) > 0)
+  if (self_contained) {
+    # Must we use "old" templates?
+    minimum_required <- "2.8"
+    installed <- as.character(rmarkdown::pandoc_version())
+    self_contained <- !(utils::compareVersion(minimum_required, installed) > 0)
+  }
 
   template_file <- paste0(name, ".html")
 
