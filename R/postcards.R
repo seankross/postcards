2430bfc80c92163e0f49c966485c0f4a28d88cae
Hadley Wickham <h.wickham@gmail.com>
2022-01-02 13:45:23 -0600

    Pass ... on
    Fixes #50

--- R/postcards.R
+++ R/postcards.R
@@ -1,36 +1,37 @@
 #' Website templates
 #'
 #' @inheritParams rmarkdown::html_document
+#' @param ... Additional arguments passed to `rmarkdown::html_document()`
 #' @export
-jolla <- function(css = NULL, includes = NULL) {
-  get_template("jolla", css, includes)
+jolla <- function(css = NULL, includes = NULL, ...) {
+  get_template("jolla", css, includes, ...)
 }
 
 #' @rdname jolla
 #' @export
-jolla_blue <- function(css = NULL, includes = NULL) {
-  get_template("jolla-blue", css, includes)
+jolla_blue <- function(css = NULL, includes = NULL, ...) {
+  get_template("jolla-blue", css, includes, ...)
 }
 
 #' @rdname jolla
 #' @export
-trestles <- function(css = NULL, includes = NULL) {
-  get_template("trestles", css, includes)
+trestles <- function(css = NULL, includes = NULL, ...) {
+  get_template("trestles", css, includes, ...)
 }
 
 #' @rdname jolla
 #' @export
-onofre <- function(css = NULL, includes = NULL) {
-  get_template("onofre", css, includes)
+onofre <- function(css = NULL, includes = NULL, ...) {
+  get_template("onofre", css, includes, ...)
 }
 
 #' @rdname jolla
 #' @export
-solana <- function(css = NULL, includes = NULL) {
-  get_template("solana", css, includes)
+solana <- function(css = NULL, includes = NULL, ...) {
+  get_template("solana", css, includes, ...)
 }
 
-get_template <- function(name, css, includes) {
+get_template <- function(name, css, includes, ...) {
 
   # Must we use "old" templates?
   minimum_required <- "2.8"
@@ -46,6 +47,7 @@ get_template <- function(name, css, includes) {
     template = system.file("pandoc_templates", template_file, package = "postcards"),
     css = css,
     includes = includes,
-    md_extensions = "-autolink_bare_uris"
+    md_extensions = "-autolink_bare_uris",
+    ...
   )
 }
